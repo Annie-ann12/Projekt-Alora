eb84c42147f8e96f75bfdc9e30fd20b27193be5b
Annie-ann12 <117024184+Annie-ann12@users.noreply.github.com>
2023-12-14 18:48:20 +0100

    dd

--- 0.0.2/script.js
+++ 0.0.2/script.js
@@ -44,5 +44,60 @@ function sortProducts(sortingOption) {
     // Add your sorting logic here based on the selected option
     console.log("Sorting products by: " + sortingOption);
 }
+// razeni
+var products = [
+    { price: 4500, name: 'Třpytivý zásnubní prsten' },
+    { price: 5440, name: 'Zásnubní prsten pro princeznu' },
+    // Add more products as needed
+];
 
-  
+// Initial product rendering
+renderProducts(products);
+
+function toggleSortingOptions() {
+    var sortingOptions = document.getElementById('sortingOptions');
+    sortingOptions.style.display = (sortingOptions.style.display === 'none' || sortingOptions.style.display === '') ? 'block' : 'none';
+}
+
+function sortProducts(sortBy) {
+    if (sortBy === 'price_ascending') {
+        products.sort((a, b) => a.price - b.price);
+    } else if (sortBy === 'price_descending') {
+        products.sort((a, b) => b.price - a.price);
+    }
+
+    renderProducts(products);
+}
+function renderProducts(products) {
+    var productsList = document.getElementById('productsList');
+    productsList.innerHTML = '';
+
+    products.forEach(product => {
+        var li = document.createElement('li');
+        li.setAttribute('data-price', product.price);
+
+        var formattedPrice = product.price.toLocaleString('cs-CZ', { style: 'currency', currency: 'CZK' });
+
+        li.innerHTML = `
+            <div class="prod_img_smaller">
+                <a href="#">
+                    <img class="product_img" src="${getImageFileName(product.name)}" alt="${product.name}">
+                </a>
+                <div class="p_a_h">
+                    <h3>
+                        <a href="#">${product.name}</a>
+                    </h3>
+                    <p>${formattedPrice}</p>
+                </div>
+            </div>
+        `;
+
+        productsList.appendChild(li);
+    });
+}
+
+// Helper function to generate a suitable image file name based on the product name
+function getImageFileName(productName) {
+    // Replace spaces with underscores and convert to lowercase
+    return productName.replace(/\s+/g, '_').toLowerCase() + '.jpg';
+}
